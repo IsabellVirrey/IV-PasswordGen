0f9ef8814f847e115e4a93f03d89befa9807b7aa
virrey95isabell@gmail.com <virrey95isabell@gmail.com>
2023-11-27 14:24:59 -0800

    modified the writePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -3,39 +3,43 @@
 
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var symbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]; //add more symbols 
 
 var characters = 8; //the longest length the password can be
 
-var password = " ";
+var CharacterChoice = [];
 
 
-//var finalPassword = lowercase + uppercase + numeric + symbol;
-
-//adding the pop up windows 
-
-//declaring the function
+var generateBtn = document.querySelector("#generate"); //step 1 
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword);
+generateBtn.addEventListener("click", writePassword); //step 2
 
 
 // Write password to the #password input
 function writePassword() {
   var CorrectQuestions = Questions(); //returns true or false 
+  var passwordText = document.querySelector("#password");
 
   if (CorrectQuestions){ //I only want this to happen if the user answered all of the quesitons correctly
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-    
-    passwordText.value = password;
+    var FinalPassword = generatePassword();
+    passwordText.value = FinalPassword;
+  } else {
+    passwordText.value = "";
   }
+
 
 }
 
 function generatePassword() {
+  var password = " ";
+  for (var i = 0; i < characters; i++) {
+    var RandomCharacter = Math.floor(Math.random() * CharacterChoice.length); 
+    password = password + CharacterChoice[RandomCharacter];
+  }
+  return password;
   //let password = " ";
   //password += lowercase[Math.floor(Math.random() * lowercase.length)];
   //password += uppercase[Math.floor(Math.random() * uppercase.length)];
@@ -49,13 +53,15 @@ function generatePassword() {
 }
 
 function Questions(){
-  password = " ";
+  CharacterChoice = "";
   characters = parseInt(prompt("How many characters do you want your password to be? Please select between 8-28 characters."));
 
   if (isNaN(characters) || characters < 8 || characters > 128) { //this should be all false
   alert("Error: please select a number between 8 and 128");
   return false;
   }
-
+if (confirm("Confirm if you would like lowercase letters?")){
+  CharacterChoice = CharacterChoice.concat(lowercase);
+}
   return true;
 }
