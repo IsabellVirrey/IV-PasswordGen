199b3725f289d7076b0fa4a39a4c84cfea89e916
virrey95isabell@gmail.com <virrey95isabell@gmail.com>
2023-11-27 10:21:26 -0800

    Declared some variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,22 @@
 // Assignment code here
+//window.alert("Pick a password at least 8 characters and no more than 128 characters");
 
+//declaring the variables
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+
+var uppercase = confirm("Your password should have an uppercase letter! Click OK to continue");
+var lowercase = confirm("Your password should have an lowercase letter! Click OK to continue");
+var numeric = confirm("Your password should have a number! Click OK to continue");
+var symbol = confirm("Your password should have a special symbol: !@#$%^&*()  Click OK to continue");
+
+//declaring the function
+function generatePassword() {
+  //function body
+  //code to be executed
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
